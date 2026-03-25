62acb85eafd0ba118e859af3a45444ce95c37cde
Billy Murphy <G00465240@atu.ie>
2026-03-25 14:16:14 +0000

    Initial commit

---
+++ src/main/java/ie/atu/week10/week10_25_03_26/Week10250326Application.java
@@ -0,0 +1,13 @@
+package ie.atu.week10.week10_25_03_26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10250326Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10250326Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week10/week10_25_03_26/Week10250326ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week10.week10_25_03_26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10250326ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
